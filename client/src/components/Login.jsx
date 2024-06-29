546e33fe56a61d9eabd8e1c12e1ff9c1abcf2141
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-06-30 00:35:36 +0530

    .

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiGlobe } from "react-icons/ci";
 import { FaAngleDown } from "react-icons/fa";
 import ReCAPTCHA from "react-google-recaptcha";
 
 const Login = () => {
 
+  const [recaptchValue, setRecaptchValue] = useState('')
+
   const clientSiteKey = import.meta.env.VITE_CAPTCHA_CLIENT_SITE_KEY;
 
   function onChange(value) {
-    console.log("Captcha value:", value);
+    setRecaptchValue(value);
   }
 
   return (
@@ -65,13 +67,27 @@ const Login = () => {
                 </svg>
                 Sign in with Google
               </button>
+              <button
+                type="button"
+                className="text-white bg-green-800 hover:bg-green-800/90  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2"
+              >
+              
+                Sign in with Okta
+              </button>
+              <button
+                type="button"
+                className="text-white bg-slate-600 hover:bg-slate-600/90  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2"
+              >
+               
+                Sign in with OneLogin
+              </button>
             </div>
             <div className="flex items-center justify-center">
               <div className="flex-grow border-t border-gray-300 w-40"></div>
               <span className="mx-4 text-gray-500">OR</span>
               <div className="flex-grow border-t border-gray-300 w-40"></div>
             </div>
-            <form onSubmit={(e) => e.preventDefault()} className="w-[50%]">
+            <form onSubmit={() => {}} className="w-[50%]">
               <div>
                 <label
                   htmlFor="email-input"
