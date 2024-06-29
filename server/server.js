92d416056793cf7f36a1501b88c89f9977d6c985
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-06-30 01:06:58 +0530

    db con done

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config({
+  path: './.env'
+})
 
 const server = express()
-const PORT = 5000
+const PORT = process.env.PORT || 8000
 
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
 server.use(cors())
 
 server.get('/', (req, res) => {
   res.send('Serving Running!')
 })
 
-server.listen(PORT, () => {
-  console.log('server running on port http://localhost:' + PORT)
-})
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {})
+    console.log('MongoDB connected successfully')
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message)
+    process.exit(1)
+  }
+}
+
+connectDB()
+
+if (connectDB) {
+  server.listen(PORT, () => {
+    console.log('server running on port http://localhost:' + PORT)
+  })
+}
